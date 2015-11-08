620ed91c2f7f267f96e78f3c13beb593d7f6f392
Dohyoung Lee <netrance@naver.com>
2015-11-09 01:59:34 +0900

    New Example - Ex06
    This example was made to show how a client and a server communicate each
    other. I supposed that the client request conversion from won to dollar
    and a server responds to it.

---
+++ AspServerExample/Ex06_Exchange/Ex06_ExchangeClient.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    // Input data as JSON object to send to the server.
+    var jsonInput = { dollar: 1 };
+
+    // Send a value as won, and receive another value as dollar.
+    $.ajax({
+        type: 'POST',
+        url: './Ex06_ExchangeServer.aspx', //'http://localhost/HelloWorld.aspx',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify(jsonInput),
+        success: function (data) {
+            document.write('JSON input - ' + jsonInput.dollar + '달러');
+            document.write('<br/>');
+            document.write('JSON output - ' + data.won + '원');
+        },
+        error: function (request, status, error) {
+            document.write('Error Code: ' + request.status + '<p/>');
+            document.write('Message: <br/>')
+            document.write(request.responseText + '<p/>');
+            document.write('Error:' + error);
+        }
+    });
+});
---
+++ AspServerExample/Ex06_Exchange/Ex06_ExchangeServer.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Ex06_ExchangeServer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Get the JSON input object from a client.
+        Hashtable jsonInput = JSONUtil.GetJSONObject(Request);
+
+        // Read input data from jsonInput.
+        long dollar = Convert.ToInt64(jsonInput["dollar"]);
+
+        // Create output data.
+        long won = convertDollarToWon(dollar);
+
+        // Create output JSON object.
+        Hashtable jsonOutput = new Hashtable();
+        jsonOutput.Add("won", won);
+        jsonOutput.Add("dollar", dollar);
+
+        // Send the output JSON object to the client.
+        // Serializing is needed to transform the object to a string.
+        Response.Write(JSONUtil.toStringFromJSON(jsonOutput));
+        Response.End();
+    }
+
+    // Suppose 1 dollar equals to 1128 wons.
+    long convertDollarToWon(long dollar)
+    {
+        return dollar * 1128;
+    }
+}
+/*
+{
+    // Temp data to test the communication between this server and clients.
+    private Hashtable[] members;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Get JSON string received from a client.
+        string json;
+        using (StreamReader reader = new StreamReader(Request.InputStream))
+        {
+            json = reader.ReadToEnd();
+        }
+
+        // Convert JSON string to Hashtable object.
+        Hashtable jsonInput = new JavaScriptSerializer().Deserialize<Hashtable>(json);
+
+        // Load or save data(if required).
+        Hashtable[] members = getMembers();
+        Hashtable targetMember = getMember(Convert.ToString(jsonInput["name"]), members);
+
+        // Create new Hashtable object to be returned to the client.
+        // Add the loaded data, which the client requested, into the container.
+        Hashtable responseContent = new Hashtable();
+        if (null != targetMember)
+        {
+            responseContent.Add("name", targetMember["name"]);
+            responseContent.Add("score", targetMember["score"]);
+        }
+
+        // Send the new Hashtable object to the client.
+        // Serializing is needed to transform the Hashtable object to a string.
+        JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+        Response.Write(jsSerializer.Serialize(responseContent));
+        Response.End();
+    }
+
+    private Hashtable[] getMembers()
+    {
+        Hashtable[] members = new Hashtable[5];
+
+        for (int i = 0; i < members.Length; i++)
+        {
+            members[i] = new Hashtable();
+        }
+
+        members[0].Add("name", "Tom");
+        members[0].Add("score", 83);
+        members[1].Add("name", "Jane");
+        members[1].Add("score", 91);
+        members[2].Add("name", "Yuri");
+        members[2].Add("score", 76);
+        members[3].Add("name", "Ryan");
+        members[3].Add("score", 73);
+        members[4].Add("name", "Jose");
+        members[4].Add("score", 95);
+
+        return members;
+    }
+
+    private Hashtable getMember(string name, Hashtable[] members)
+    {
+        foreach (Hashtable ht in members)
+        {
+            if (!ht.ContainsKey("name"))
+            {
+                continue;
+            }
+
+            if (name.Equals(ht["name"]))
+            {
+                return ht;
+            }
+        }
+
+        return null;
+    }
+}
+*/
